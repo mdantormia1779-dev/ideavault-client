e47f31ac3f07c96fc777d49751b3c34dffacbd98
Md Antor Mia <your-email@gmail.com>
2026-05-22 12:00:43 +0600

    authentication fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,25 +3,30 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.DB_URI);
+
+// 🔥 MUST CONNECT
+await client.connect();
+
 const db = client.db("idea_vault");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
   }),
+
   emailAndPassword: {
     enabled: true,
   },
+
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
   },
-  trustedOrigins: [
-        "http://localhost:3000", 
-        "https://ideavault-client-tawny.vercel.app"
-    ]
 
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://ideavault-client-tawny.vercel.app",
+  ],
 });
